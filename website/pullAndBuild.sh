32e0c02e9900ade3a6f0ea3d073b3beb035d58d1
bawjensen <bryanawjensen@gmail.com>
2015-02-04 10:57:42 -0500

    Adding step to clean out old session data - possibly remove in future

--- website/pullAndBuild.sh
+++ website/pullAndBuild.sh
@@ -2,4 +2,6 @@ git pull
 npm install
 node cronjobs/cron.hourly/*-cron.js
 node cronjobs/cron.daily/*-cron.js
+echo "Deleting all old session data"
+rm sub-projects/wave/data/schedule-data/*.json
 echo "Don't forget to restart the server"
